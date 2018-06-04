2b1b0c6703da27ffbb2767d1f2f7e9542571b9e8
npristin <pristin.nicole@gmail.com>
2018-06-03 22:52:13 -0400

    Add App const containing the necessary components and elements

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native';
+import FixedHeader from './elements/FixedHeader'
+import TextHeadings from './elements/TextHeadings'
+import Icons from './elements/Icons'
+import Exam from './elements/Exam'
+import QuestionTypeButtonGroupChooser from './elements/QuestionTypeButtonGroupChooser'
+import QuestionTypePicker from './elements/QuestionTypePicker'
+import TrueFalseQuestionEditor from './elements/TrueFalseQuestionEditor'
+import MultipleChoiceQuestionEditor from './elements/MultipleChoiceQuestionEditor'
+import EssayQuestionEditor from './elements/EssayQuestionEditor'
+import { createStackNavigator } from 'react-navigation'
+import {Button} from 'react-native-elements'
+import ScreenX from './elements/ScreenX'
+import CourseList from './components/CourseList'
+import ModuleList from './components/ModuleList'
+import LessonList from './components/LessonList'
+import WidgetList from './components/WidgetList'
+import QuestionList from './components/QuestionList'
+
+
+const App = createStackNavigator({
+  Home,
+  CourseList,
+  ModuleList,
+  LessonList,
+  WidgetList,
+  QuestionList,
+  TrueFalseQuestionEditor,
+  MultipleChoiceQuestionEditor,
+  EssayQuestionEditor
+});
+
+export default App;
