15b2625deb8b1f13e4754aee6ee2d265915a4e71
npristin <pristin.nicole@gmail.com>
2018-06-03 23:18:52 -0400

    Render the multiple choice questino editor

--- elements/MultipleChoiceQuestionEditor.js
+++ elements/MultipleChoiceQuestionEditor.js
@@ -18,6 +18,44 @@ class MultipleChoiceQuestionEditor extends React.Component {
   updateForm(newState) {
     this.setState(newState)
   }
+  render() {
+    return(
+      <View>
+        <FormLabel>Title</FormLabel>
+        <FormInput onChangeText={
+          text => this.updateForm({title: text})
+        }/>
+        <FormValidationMessage>
+          Title is required
+        </FormValidationMessage>
+
+        <FormLabel>Description</FormLabel>
+        <FormInput onChangeText={
+          text => this.updateForm({description: text})
+        }/>
+        <FormValidationMessage>
+          Description is required
+        </FormValidationMessage>
+
+        <FormLabel>Choices</FormLabel>
+        <FormInput onChangeText={
+          text => this.updateForm({options: text})
+        }/>
+
+        <Button	backgroundColor="green"
+                 color="white"
+                 title="Save"/>
+        <Button	backgroundColor="red"
+                 color="white"
+                 title="Cancel"/>
+
+        <Text h3>Preview</Text>
+        <Text h2>{this.state.title}</Text>
+        <Text>{this.state.description}</Text>
+
+      </View>
+    )
+  }
 }
 
 export default MultipleChoiceQuestionEditor
