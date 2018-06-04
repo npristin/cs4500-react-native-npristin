67d3c2cb886655f45f88513ea28d162204d98a6a
npristin <pristin.nicole@gmail.com>
2018-06-03 23:23:12 -0400

    Add component did mount to widget list

--- components/WidgetList.js
+++ components/WidgetList.js
@@ -13,5 +13,15 @@ class WidgetList extends Component {
       lessonId: 1
     }
   }
+  componentDidMount() {
+    const {navigation} = this.props;
+    const lessonId = navigation.getParam("lessonId")
+    this.setState({lessonId: lessonId})
+    fetch("https://cs4550-java-server-npristin.herokuapp.com/api/widget")
+      .then(response => (response.json()))
+      .then(widgets => this.setState(
+        {widgets: widgets.filter(w => w.lessonId == lessonId && w.className == null)}))
+  }
+
 }
 export default WidgetList
