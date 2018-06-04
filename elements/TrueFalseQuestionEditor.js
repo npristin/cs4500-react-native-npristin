fc89407134b08fced0b0d23dac38968576af8816
npristin <pristin.nicole@gmail.com>
2018-06-03 23:19:18 -0400

    Add true false questino editor class

---
+++ elements/TrueFalseQuestionEditor.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {View} from 'react-native'
+import {Text, Button, CheckBox} from 'react-native-elements'
+import {FormLabel, FormInput, FormValidationMessage}
+  from 'react-native-elements'
+
+class TrueFalseQuestionEditor extends React.Component {
+  static navigationOptions = { title: "True False"}
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      description: '',
+      points: 0,
+      isTrue: true
+    }
+  }
+}
+
+export default TrueFalseQuestionEditor
