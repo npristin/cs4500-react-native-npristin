07b5f3385bc8a9268ddb22d9ca7fcdfb22cac501
npristin <pristin.nicole@gmail.com>
2018-06-04 23:43:20 -0400

    Add functionality for creating a true false question

--- elements/TrueFalseQuestionEditor.js
+++ elements/TrueFalseQuestionEditor.js
@@ -9,47 +9,92 @@ class TrueFalseQuestionEditor extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      title: '',
-      description: '',
-      points: 0,
-      isTrue: true
+        question: {
+          title: '',
+          description: '',
+          points: 0,
+          isTrue: true
+      },
+      examId: 1
     }
   }
+
+  componentDidMount() {
+      const {navigation} = this.props;
+      const examId = navigation.getParam("examId")
+      const questionId = navigation.getParam("questionId")
+      this.setState({examId: examId})
+      console.log(this.state.examId)
+      console.log(questionId)
+
+      if (questionId != null) {
+          fetch("https://cs4550-java-server-npristin.herokuapp.com/api/truefalse/" + questionId)
+          .then(response => (response.json()))
+          .then(question => this.setState({question: question}))
+      }
+   }
+
   updateForm(newState) {
     this.setState(newState)
   }
+
+  createTrueFalseQuestion() {
+      console.log("creating true false question")
+      console.log(this.state.question)
+      fetch("https://cs4550-java-server-npristin.herokuapp.com/api/exam/" + this.state.examId + "/truefalse", {
+              body: JSON.stringify(this.state.question),
+              headers: { 'Content-Type': 'application/json'},
+              method: 'POST'
+      })
+    }
+
   render() {
     return(
       <View>
         <FormLabel>Title</FormLabel>
-        <FormInput onChangeText={
-          text => this.updateForm({title: text})
+        <FormInput
+            value={this.state.question.title}
+            onChangeText={
+                text => this.updateForm({question: {...this.state.question, title: text }})
         }/>
         <FormValidationMessage>
           Title is required
         </FormValidationMessage>
 
         <FormLabel>Description</FormLabel>
-        <FormInput onChangeText={
-          text => this.updateForm({description: text})
+        <FormInput
+            value={this.state.question.description}
+            onChangeText={
+                text => this.updateForm({question: {...this.state.question, description: text}})
         }/>
         <FormValidationMessage>
           Description is required
         </FormValidationMessage>
 
-        <CheckBox onPress={() => this.updateForm({isTrue: !this.state.isTrue})}
-                  checked={this.state.isTrue} title='The answer is true'/>
+        <FormLabel>Points</FormLabel>
+        <FormInput
+            value={this.state.question.points.toString()}
+            onChangeText={
+                text => this.updateForm({question: {...this.state.question, points: text}})
+        }/>
+        <FormValidationMessage>
+          Points are required
+        </FormValidationMessage>
+
+        <CheckBox onPress={() => this.updateForm({question: {isTrue: !this.state.isTrue}})}
+                  checked={this.state.question.isTrue} title='The answer is true'/>
 
         <Button	backgroundColor="green"
                  color="white"
-                 title="Save"/>
+                 title="Save"
+                 onPress={() => this.createTrueFalseQuestion()}/>
         <Button	backgroundColor="red"
                  color="white"
                  title="Cancel"/>
 
         <Text h3>Preview</Text>
-        <Text h2>{this.state.title}</Text>
-        <Text>{this.state.description}</Text>
+        <Text h2>{this.state.question.title}</Text>
+        <Text>{this.state.question.description}</Text>
 
       </View>
     )
