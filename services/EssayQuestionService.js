f0660611f755a2c26c1bc92b3735e350338fdf29
npristin <pristin.nicole@gmail.com>
2018-06-06 02:03:00 -0400

    Create service for essay questions

---
+++ services/EssayQuestionService.js
@@ -0,0 +1,21 @@
+let _singleton = Symbol();
+
+const ESSAY_API_URL = 'https://cs4550-java-server-npristin.herokuapp.com/api/essay/';
+const ESSAY_EID_API_URL = 'https://cs4550-java-server-npristin.herokuapp.com/api/exam/EID/essay';
+
+export default class EssayQuestionService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken) {
+            throw new Error('Singleton essay service.')
+        }
+    }
+
+    static instance() {
+        if (!this[_singleton]) {
+            this[_singleton] = new EssayQuestionService(_singleton);
+        }
+        return this[_singleton];
+    }
+
+    
+}
