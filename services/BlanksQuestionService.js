797cde69ad65e053658175ad243c2c15649428e4
npristin <pristin.nicole@gmail.com>
2018-06-06 02:18:43 -0400

    Add a fill in the blank question service

--- services/BlanksQuestionService.js
+++ services/BlanksQuestionService.js
@@ -1,9 +1,9 @@
 let _singleton = Symbol();
 
-const ESSAY_API_URL = 'https://cs4550-java-server-npristin.herokuapp.com/api/essay';
-const ESSAY_EID_API_URL = 'https://cs4550-java-server-npristin.herokuapp.com/api/exam/EID/essay';
+const BLANKS_API_URL = 'https://cs4550-java-server-npristin.herokuapp.com/api/blanks';
+const BLANKS_EID_API_URL = 'https://cs4550-java-server-npristin.herokuapp.com/api/exam/EID/blanks';
 
-export default class EssayQuestionService {
+export default class BlanksQuestionService {
     constructor(singletonToken) {
         if (_singleton !== singletonToken) {
             throw new Error('Singleton essay service.')
@@ -12,20 +12,20 @@ export default class EssayQuestionService {
 
     static instance() {
         if (!this[_singleton]) {
-            this[_singleton] = new EssayQuestionService(_singleton);
+            this[_singleton] = new BlanksQuestionService(_singleton);
         }
         return this[_singleton];
     }
 
     findEssayById(questionId) {
-        return fetch(ESSAY_API_URL + '/' + questionId)
+        return fetch(BLANKS_API_URL + '/' + questionId)
             .then(function(response) {
                 return response.json();
             });
     }
 
     createEssayQuestion(examId, question) {
-        return fetch(ESSAY_EID_API_URL.replace('EID', examId), {
+        return fetch(BLANKS_EID_API_URL.replace('EID', examId), {
             body: JSON.stringify(question),
             headers: { 'Content-Type': 'application/json' },
             method: 'POST'
@@ -34,7 +34,7 @@ export default class EssayQuestionService {
     }
 
     deleteEssayQuestion(questionId) {
-        return fetch(ESSAY_API_URL + '/' + questionId,
+        return fetch(BLANKS_API_URL + '/' + questionId,
           {
             method: 'DELETE'
           });
